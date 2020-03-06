4b0b4f466fa8b814c2e45f1660240a4da0a44adc
Mahmoud <Metainy@users.noreply.github.com>
2020-03-06 08:16:08 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,438 @@
+/**
+ * All heroes information
+ */
+let heroes = {
+
+    // Season 0 Heroes
+    Razor : {
+        "Name": "Razor: Musk",
+        "Image": "<img src='https://1.bp.blogspot.com/-ckTfP-KZRH4/XkQt_dMnh0I/AAAAAAAAAGs/Z7Ud559mKEAyMaPhTas0fuqdK8PH-fWawCLcBGAsYHQ/s320/MEmu_aLGhC9rpWR-removebg-preview.png'>",
+        "Season": "S0",
+        "Type": "Vehicles",
+        "Position": "2",
+        "Abilities": "-"
+    },
+    Hans : {
+        "Name": "Iron Guard: Hans",
+        "Image": "<img src='https://1.bp.blogspot.com/-8-8OCxV4Ppc/XkQt9AfYzKI/AAAAAAAAAGg/6KLEqzC_NlYfZkSfIW7-U-HiENhmU21OwCLcBGAsYHQ/s320/MEmu_FWvXo8CM68-removebg-preview.png'>",
+        "Season": "S0",
+        "Type": "Shooters",
+        "Position": "2, 3",
+        "Abilities": "Attacker"
+    },
+    Militant : {
+        "Name": "Militant: Nile",
+        "Image": "<img src='https://1.bp.blogspot.com/-4K7zX7x5cQk/XkQuBu_GnuI/AAAAAAAAAG0/DPyrSITIedw18RZyvGzZxzsCkfOWjapzgCLcBGAsYHQ/s320/MEmu_ejm1Fe55Kh-removebg-preview.png'>",
+        "Season": "S0",
+        "Type": "Fighters",
+        "Position": "1 , 2",
+        "Abilities": "Silence (Enemy unable skill attack)"
+    },
+    Forsaken : {
+        "Name": "The Forsaken One",
+        "Image": "<img src='https://1.bp.blogspot.com/-HwH6tYm_eB8/XkQuB-eW3tI/AAAAAAAAAG4/VMSs_RPhaTIkfOvFH4CGT4RR4ng9rtvnQCLcBGAsYHQ/s1600/MEmu_fgBmohKiZj-removebg-preview.png'>",
+        "Season": "S0",
+        "Type": "All",
+        "Position": "1",
+        "Abilities": "Silence (Enemy unable skill attack), Weaken"
+    },
+    Arsenal : {
+        "Name": "Sergej The Arsenal",
+        "Image": "<img src='https://1.bp.blogspot.com/-vuavJFDYrkc/XkQt8An7pdI/AAAAAAAAAGY/dpaPEjqam0sK_4J2XlOVNgyilu-wmMQjwCLcBGAsYHQ/s320/MEmu_CNuqEp3bPZ-removebg-preview.png'>",
+        "Season": "S0",
+        "Type": "All",
+        "Position": "2",
+        "Abilities": "Defender, Suppression (Enemy unable basic / skill attack)"
+    },
+    ShootaMan : {
+        "Name": "Shoota Man: Mohauuk",
+        "Image": "<img src='https://1.bp.blogspot.com/-__SeUNNO6QA/XkQt_KfDIRI/AAAAAAAAAGk/pzHrWZNhweoMa8AfZO47D-X3-CXr3l07gCLcBGAsYHQ/s320/MEmu_H9vZiKdfRk-removebg-preview.png'>",
+        "Season": "S0",
+        "Type": "All",
+        "Position": "3",
+        "Abilities": "Attacker"
+    },
+    Destroya : {
+        "Name": "Iron Teef The Destroya",
+        "Image": "<img src='https://1.bp.blogspot.com/-qzkRF2buWns/XkQuBAVWyvI/AAAAAAAAAGw/YZm7PiSEUrc3RJDnYSLiO8SyHIXsiGJtQCLcBGAsYHQ/s320/MEmu_cgay5sd8Ia-removebg-preview.png'>",
+        "Season": "S0",
+        "Type": "All",
+        "Position": "1",
+        "Abilities": "-"
+    },
+    Guardian : {
+        "Name": "Dawn Guardian: An'nuo",
+        "Image": "<img src='https://1.bp.blogspot.com/-WPGh1sAFy_k/XkQt_GGV-SI/AAAAAAAAAGo/dI9gmlXVtmMpm4pbwfAroGPaBJSKb9argCLcBGAsYHQ/s320/MEmu_TlRSTIMjhf-removebg-preview.png'>",
+        "Season": "S0",
+        "Type": "All",
+        "Position": "3",
+        "Abilities": "Attacker, Pierced Armor"
+    },
+    Reaper : {
+        "Name": "Reaper",
+        "Image": "<img src='https://1.bp.blogspot.com/-6Dya89CS8Bs/XkQt8UvadmI/AAAAAAAAAGc/QfkMnMtogcwtNWux8-_pKM_Vopfc59tmACLcBGAsYHQ/s320/MEmu_6Wb0SoND45-removebg-preview.png'>",
+        "Season": "S0",
+        "Type": "All",
+        "Position": "2",
+        "Abilities": "Attacker"
+    },
+    WarHound : {
+        "Name": "Adane The War Hound",
+        "Image": "<img src='https://1.bp.blogspot.com/-GAzjkvadShU/XkQt7aWKJhI/AAAAAAAAAGU/t_tYABngEg0X-CaT24QjKquqymF-NnEQwCLcBGAsYHQ/s320/MEmu_5Dw9ClOKik-removebg-preview.png'>",
+        "Season": "S0",
+        "Type": "All",
+        "Position": "2",
+        "Abilities": "Blunt Blade, Defender, Weaken"
+    },
+
+    // Season 1 Heroes
+    Dragon : {
+        "Name": "Dragon's Rage: Danny",
+        "Image": "<img src='https://1.bp.blogspot.com/-lUub772u-vU/XkbfFviRBoI/AAAAAAAAAHk/qjezszIUMOojMPDLksnpYEfj6PPWfytiwCLcBGAsYHQ/s320/MEmu_8Eyk61johV-removebg-preview.png'>",
+        "Season": "S1",
+        "Type": "Fighters",
+        "Position": "3",
+        "Abilities": "Suppression (Enemy unable basic / skill attack)"
+    },
+    Chuck : {
+        "Name": "War Boss: Chuck",
+        "Image": "<img src='https://1.bp.blogspot.com/-NT1Ge_G_j-s/XkbfGF0CXnI/AAAAAAAAAHo/YqYUNMVwSywcsPza4Ncr6COYzPuWAH87wCLcBGAsYHQ/s320/MEmu_GCbNWFJUQ3-removebg-preview.png'>",
+        "Season": "S1",
+        "Type": "Vehicles",
+        "Position": "3",
+        "Abilities": "Weaken"
+    },
+    Janissary : {
+        "Name": "Fatih The Janissary",
+        "Image": "<img src='https://1.bp.blogspot.com/-SdGW1aGSdQY/XkbfGFj5CzI/AAAAAAAAAHs/h7gloY3oiwQFFKzTMTY3vfJZQ4LzOyJkwCLcBGAsYHQ/s320/MEmu_86G0fEMs6K-removebg-preview.png'>",
+        "Season": "S1",
+        "Type": "Shooters",
+        "Position": "1",
+        "Abilities": "Attacker"
+    },
+    Falcon : {
+        "Name": "The Falcon: Khalid",
+        "Image": "<img src='https://1.bp.blogspot.com/-IjbepfSpO-Q/XkbfINzPKjI/AAAAAAAAAH0/f1On_vjFN6AGyWTtkamcRHB-jprtcC9YgCLcBGAsYHQ/s320/MEmu_gh6KJ3H76f-removebg-preview.png'>",
+        "Season": "S1",
+        "Type": "Vehicles",
+        "Position": "1",
+        "Abilities": "-"
+    },
+    Courageous : {
+        "Name": "Uqbah The Courageous",
+        "Image": "<img src='https://1.bp.blogspot.com/-7h7RxJGE38c/XkbfIJ-x2hI/AAAAAAAAAH4/V839ae70TO8-UJT3hvUyIxeUrTOos9EIACLcBGAsYHQ/s320/MEmu_iRXIrJfNgs-removebg-preview.png'>",
+        "Season": "S1",
+        "Type": "Fighters",
+        "Position": "1",
+        "Abilities": "-"
+    },
+    Klause : {
+        "Name": "Light of the past: Klause",
+        "Image": "<img src='https://1.bp.blogspot.com/-lpCqHnWSzCU/XkbfI1mRnNI/AAAAAAAAAH8/ythFPyl04VsFoBExMWAk2BQlc3Vjyg_0wCLcBGAsYHQ/s320/MEmu_lysbmjJmNg-removebg-preview.png'>",
+        "Season": "S1",
+        "Type": "Fighters",
+        "Position": "1",
+        "Abilities": "-"
+    },
+    Prof : {
+        "Name": "Part The Professional",
+        "Image": "<img src='https://1.bp.blogspot.com/-hdtCdpgux58/XkbfKQoT6bI/AAAAAAAAAIE/0dZwwQkP1awdM3873xfl5cd_fzwIRAdHQCLcBGAsYHQ/s320/MEmu_mKFlkNSpwq-removebg-preview.png'>",
+        "Season": "S1",
+        "Type": "Shooters",
+        "Position": "3",
+        "Abilities": "Attacker, Suppression (Enemy unable basic / skill attack)"
+    },
+    Barnes : {
+        "Name": "The Cincinnatus: Barnes",
+        "Image": "<img src='https://1.bp.blogspot.com/-WLwcRKKdx40/XkbfK6xTgTI/AAAAAAAAAII/M8sGgFmGpIEJe8s6DJNfl-7vDaoZJvrwwCLcBGAsYHQ/s320/MEmu_wsmqlgWR1w-removebg-preview.png'>",
+        "Season": "S1",
+        "Type": "Shooters",
+        "Position": "1",
+        "Abilities": "Defender, Dodge, Massive Damage, Ferocious Damage"
+    },
+    Jax : {
+        "Name": "The Flash: Jax",
+        "Image": "<img src='https://1.bp.blogspot.com/-0h014dXYdBg/XkbfG0Wb2jI/AAAAAAAAAHw/tCdRQJqPkG4CjImVvKj1F3kRnsrVsXXUACLcBGAsYHQ/s320/MEmu_Z2vjysGIN7-removebg-preview.png'>",
+        "Season": "S1",
+        "Type": "Vehicles",
+        "Position": "1",
+        "Abilities": "Massive Damage, Ferocious Damage, Dodge"
+    },
+
+    // Season 2 Heroes
+    Nigel : {
+        "Name": "Viscount: Nigel",
+        "Image": "<img src='https://1.bp.blogspot.com/-fC1o4UnbHWU/XkfxPCpwgXI/AAAAAAAAAJA/AH3N0wcPQsM2eNYjQtzXJG36h2O6AXj2wCLcBGAsYHQ/s320/MEmu_uT8lb1gyYI-removebg-preview.png'>",
+        "Season": "S2",
+        "Type": "Fighters",
+        "Position": "1",
+        "Abilities": "Counter-Attack"
+    },
+    Andre : {
+        "Name": "The Panzer: Andre",
+        "Image": "<img src='https://1.bp.blogspot.com/--cGVTYE4e1k/XkfxPp5y3EI/AAAAAAAAAJE/IwQfYJJ5I1g1sOBAfdvtypEZM0JUmrYBACLcBGAsYHQ/s320/MEmu_x9TXaMAkmv-removebg-preview.png'>",
+        "Season": "S2",
+        "Type": "Vehicles",
+        "Position": "1",
+        "Abilities": "Counter-Attack, Splash / Sputtering"
+    },
+    Jorge : {
+        "Name": "The Inquisitor: Jorge",
+        "Image": "<img src='https://1.bp.blogspot.com/-obJvpwkZEfI/XkfxPPNgzYI/AAAAAAAAAI8/qKj7GNSGgeYD5DNDY6hgWLqfxb5ZrA1CgCLcBGAsYHQ/s320/MEmu_rTNwnKmveY-removebg-preview.png'>",
+        "Season": "S2",
+        "Type": "Shooters",
+        "Position": "2",
+        "Abilities": "Attacker, Disarm (Enemy unable basic attack), Suppression (Enemy unable basic/skill attack)"
+    },
+    Eudora : {
+        "Name": "The Executioner: Eudora",
+        "Image": "<img src='https://1.bp.blogspot.com/-HGr6Ce_8STU/XkfxMKPiVQI/AAAAAAAAAIw/1-zzLlWEluoZ5DUo-Pb79seeE82MIGNxQCLcBGAsYHQ/s320/MEmu_r9ryuEIv6k-removebg-preview.png'>",
+        "Season": "S2",
+        "Type": "Shooters",
+        "Position": "2, 3",
+        "Abilities": "Attacker, Burning / Flammable / Ignite, Silence (Enemy unable skill attack)"
+    },
+    Jane : {
+        "Name": "Knight: Jane",
+        "Image": "<img src='https://1.bp.blogspot.com/-S6MluEQoCa4/XkfxMb7na1I/AAAAAAAAAI4/bIm6YiUdjNQOxjtwbHLKxGgCJMqsChFYgCLcBGAsYHQ/s320/MEmu_JrJaPAyjLZ-removebg-preview.png'>",
+        "Season": "S2",
+        "Type": "Fighters",
+        "Position": "2, 3",
+        "Abilities": "Curse"
+    },
+    Captain : {
+        "Name": "Captain Ivanov",
+        "Image": "<img src='https://1.bp.blogspot.com/-OMynYA21D28/XkfxMcalrFI/AAAAAAAAAI0/RUTtfRero1IbcyxT9dS6oZphVnjusN2igCLcBGAsYHQ/s320/MEmu_gjIOxUq157-removebg-preview.png'>",
+        "Season": "S2",
+        "Type": "Vehicles",
+        "Position": "2, 3",
+        "Abilities": "Silence (Enemy unable skill attack)"
+    },
+
+    // Season 3 Heroes
+    Wings : {
+        "Name": "Wings of Liberty: Jason Van Dyck",
+        "Image": "<img src='https://1.bp.blogspot.com/-vnjrnF5jgNY/XlJtA663sjI/AAAAAAAAALI/ksY5Qfp7UPkx1dtRgWsfJpmwtT0vJ8TogCLcBGAsYHQ/s320/MEmu_eybFkUROPn-removebg-preview.png'>",
+        "Season": "S3",
+        "Type": "Shooters",
+        "Position": "2",
+        "Abilities": "Defender, Disarm (Enemy unable basic attack), First-Aid / Recover"
+    },
+    Vanguard : {
+        "Name": "Vanguard: Iharakayo",
+        "Image": "<img src='https://1.bp.blogspot.com/-lGX6O2rfLDU/XlJs-stHS8I/AAAAAAAAAK4/6PNKHeX9SwQHIVxM0hSH-B_J_6Coske4ACLcBGAsYHQ/s320/MEmu_BDAY5Q5io2-removebg-preview.png'>",
+        "Season": "S3",
+        "Type": "Vehicles",
+        "Position": "1",
+        "Abilities": "Evade / Evasion"
+    },
+    Lee : {
+        "Name": "Tech Priestess: Jiwon Lee",
+        "Image": "<img src='https://1.bp.blogspot.com/-50YbPX70C7c/XlJs_uAGN8I/AAAAAAAAALA/YEq2jzV6ptgwcC5bGxoy0_PvixO-F1jFACLcBGAsYHQ/s320/MEmu_Rg6ld0ThJw-removebg-preview.png'>",
+        "Season": "S3",
+        "Type": "Fighters",
+        "Position": "2",
+        "Abilities": "Confuse / Confusion, First-Aid / Recover"
+    },
+    Anastasia : {
+        "Name": "Major Anastasia Nastya",
+        "Image": "<img src='https://1.bp.blogspot.com/-jffH3Zufiew/XlJs-kHsRGI/AAAAAAAAAK8/X5aWl730fUIZLtlpX7cO0UsmaxsHorm1QCLcBGAsYHQ/s320/MEmu_Ookrj6vQIS-removebg-preview.png'>",
+        "Season": "S3",
+        "Type": "Vehicles",
+        "Position": "3",
+        "Abilities": "Suppression (Enemy unable basic / skill attack)"
+    },
+    Farseer : {
+        "Name": "Farseer: Yuu'Chen Haa'm",
+        "Image": "<img src='https://1.bp.blogspot.com/-yVyR4J1Z56Y/XlJs-TuNIQI/AAAAAAAAAK0/_RD9KR0DBvE6P8ZMZYG2ccldYEj6kTeMwCLcBGAsYHQ/s320/MEmu_MJSEsZfg6q-removebg-preview.png'>",
+        "Season": "S3",
+        "Type": "Vehicles",
+        "Position": "2",
+        "Abilities": "Clarity / Sober"
+    },
+    Veronica : {
+        "Name": "Countdown: Veronica",
+        "Image": "<img src='https://1.bp.blogspot.com/-EgW-SVkIVsQ/XlJtAUSbp2I/AAAAAAAAALE/1LfEqWuldcILhViE52Agtz42N7fO25udgCLcBGAsYHQ/s320/MEmu_X5deOr9nyi-removebg-preview.png'>",
+        "Season": "S3",
+        "Type": "Vehicles",
+        "Position": "3",
+        "Abilities": "-"
+    },
+
+    // Season 4 Heroes
+    Bird : {
+        "Name": "Humming Bird: Lydia",
+        "Image": "<img src='https://1.bp.blogspot.com/-m_f-W6BjmPU/XlOcBraPGBI/AAAAAAAAAL0/A0Ea5dkYBS0NRrtjsaQDUC_Fd6jVdBa0QCLcBGAsYHQ/s320/MEmu_nknhqeHx5U-removebg-preview.png'>",
+        "Season": "S4",
+        "Type": "Shooters",
+        "Position": "3",
+        "Abilities": "Defender, Disarm (Enemy unable basic attack), Massive Damage, Ferocious Damage"
+    },
+    Walker : {
+        "Name": "Venom Walker: Davoros",
+        "Image": "<img src='https://1.bp.blogspot.com/-xVbWIhjChAU/XlOcJBMvTTI/AAAAAAAAAL4/WL5ffz41FrI4Cd1o6riEXbI2PhGUCXtegCLcBGAsYHQ/s320/MEmu_VLjX7I8mRe-removebg-preview.png'>",
+        "Season": "S4",
+        "Type": "Fighters",
+        "Position": "3",
+        "Abilities": "Burning / Flammable / Ignite, Curse, First-Aid / Recover, Poison"
+    },
+    Juggernaut : {
+        "Name": "Juggernaut: Constantine",
+        "Image": "<img src='https://1.bp.blogspot.com/-1LkGUHi_dI0/XlOcNAW7W3I/AAAAAAAAAL8/s5wCRcZlN7cPby4seQul6d9wXsbC6d_wgCLcBGAsYHQ/s320/MEmu_NeKGINyAC6-removebg-preview.png'>",
+        "Season": "S4",
+        "Type": "Fighters",
+        "Position": "1",
+        "Abilities": "Counter-Attack, First-Aid / Recover"
+    },
+    Lian : {
+        "Name": "Deus ex Machina: Lian Cheng Xie",
+        "Image": "<img src='https://1.bp.blogspot.com/-ZD_DNCX006Q/XlOcQAWiOyI/AAAAAAAAAMA/X4KNk_DPLaw0SZYarGZZ-S8Tnam_inaMgCLcBGAsYHQ/s1600/MEmu_vMhPVLjABb-removebg-preview.png'>",
+        "Season": "S4",
+        "Type": "Shooters",
+        "Position": "1",
+        "Abilities": "Attacker, First-Aid / Recover"
+    },
+    Augustine : {
+        "Name": "Captain Augustine",
+        "Image": "<img src='https://1.bp.blogspot.com/-wFTWR7OurzI/XlOcfscNfMI/AAAAAAAAAMM/3NO_IvwrIVgMiP73e7CVjboRkYc7QgD8gCLcBGAsYHQ/s1600/MEmu_3lih28ve7r-removebg-preview.png'>",
+        "Season": "S4",
+        "Type": "Shooters",
+        "Position": "1",
+        "Abilities": "Defender, Disarm (Enemy unable basic attack)"
+    },
+    Diesel : {
+        "Name": "Caesar: Diesel",
+        "Image": "<img src='https://1.bp.blogspot.com/-vkzkwUkHNoI/XlOclqEOYbI/AAAAAAAAAMU/gBt_DBYhYLwdL1GEJzhIRAxARL817hcjgCLcBGAsYHQ/s1600/MEmu_eFMCsXF5EY-removebg-preview.png'>",
+        "Season": "S4",
+        "Type": "Vehicles",
+        "Position": "1",
+        "Abilities": "Disarm (Enemy unable basic attack), Silence (Enemy unable skill attack)"
+    },
+};
+
+function changeTable1(value) {
+
+    // Show all hidden heroes
+    document.getElementById("mil2").style.display = "block";
+
+    // Checking for duplicate heroes
+    if (value === "Militant") {
+        document.getElementById("mil2").style.display = "none";
+    }
+
+    document.getElementById("td1").innerHTML = heroes[value].Image;
+    document.getElementById("td2").innerHTML = heroes[value].Name;
+    document.getElementById("td3").innerHTML = heroes[value].Season;
+    document.getElementById("td4").innerHTML = heroes[value].Type;
+    document.getElementById("td5").innerHTML = heroes[value].Position;
+    document.getElementById("td6").innerHTML = heroes[value].Abilities;
+}
+
+function changeTable2(value) {
+
+    // Show all hidden heroes
+    document.getElementById("mil1").style.display = "block";
+    document.getElementById("han3").style.display = "block";
+    document.getElementById("eudora3").style.display = "block";
+    document.getElementById("jane3").style.display = "block";
+    document.getElementById("captain3").style.display = "block";
+
+    // Checking for duplicate heroes
+    if (value === "Militant") {
+        document.getElementById("mil1").style.display = "none";
+    }
+    if (value === "Hans") {
+        document.getElementById("han3").style.display = "none";
+    }
+    if (value === "Eudora") {
+        document.getElementById("eudora3").style.display = "none";
+    }
+    if (value === "Jane") {
+        document.getElementById("jane3").style.display = "none";
+    }
+    if (value === "Captain") {
+        document.getElementById("captain3").style.display = "none";
+    }
+
+    document.getElementById("td11").innerHTML = heroes[value].Image;
+    document.getElementById("td22").innerHTML = heroes[value].Name;
+    document.getElementById("td33").innerHTML = heroes[value].Season;
+    document.getElementById("td44").innerHTML = heroes[value].Type;
+    document.getElementById("td55").innerHTML = heroes[value].Position;
+    document.getElementById("td66").innerHTML = heroes[value].Abilities;
+}
+
+function changeTable3(value) {
+
+    // Show all hidden heroes
+    document.getElementById("han2").style.display = "block";
+    document.getElementById("eudora2").style.display = "block";
+    document.getElementById("jane2").style.display = "block";
+    document.getElementById("captain2").style.display = "block";
+
+    // Checking for duplicate heroes
+    if (value === "Hans") {
+        document.getElementById("han2").style.display = "none";
+    }
+    if (value === "Eudora") {
+        document.getElementById("eudora2").style.display = "none";
+    }
+    if (value === "Jane") {
+        document.getElementById("jane2").style.display = "none";
+    }
+    if (value === "Captain") {
+        document.getElementById("captain2").style.display = "none";
+    }
+
+    document.getElementById("td111").innerHTML = heroes[value].Image;
+    document.getElementById("td222").innerHTML = heroes[value].Name;
+    document.getElementById("td333").innerHTML = heroes[value].Season;
+    document.getElementById("td444").innerHTML = heroes[value].Type;
+    document.getElementById("td555").innerHTML = heroes[value].Position;
+    document.getElementById("td666").innerHTML = heroes[value].Abilities;
+}
+
+
+const allBtn = document.querySelector("#all");
+const fightersBtn = document.querySelector("#fighters");
+const vehiclesBtn = document.querySelector("#vehicles");
+const shootersBtn = document.querySelector("#shooters");
+
+/**
+ * All button on click listener
+ */
+allBtn.addEventListener("click", function () {
+
+    document.querySelectorAll(".cFighters").forEach(el => el.style.display = "unset");
+    document.querySelectorAll(".cVehicles").forEach(el => el.style.display = "unset");
+    document.querySelectorAll(".cShooters").forEach(el => el.style.display = "unset");
+});
+
+/**
+ * Fighters button on click listener
+ */
+fightersBtn.addEventListener("click", function () {
+
+    document.querySelectorAll(".cFighters").forEach(el => el.style.display = "unset");
+    document.querySelectorAll(".cVehicles").forEach(el => el.style.display = "none");
+    document.querySelectorAll(".cShooters").forEach(el => el.style.display = "none");
+});
+
+/**
+ * Vehicles button on click listener
+ */
+vehiclesBtn.addEventListener("click", function () {
+    document.querySelectorAll(".cFighters").forEach(el => el.style.display = "none");
+    document.querySelectorAll(".cVehicles").forEach(el => el.style.display = "unset");
+    document.querySelectorAll(".cShooters").forEach(el => el.style.display = "none");
+});
+
+/**
+ * Shooters button on click listener
+ */
+shootersBtn.addEventListener("click", function () {
+
+    document.querySelectorAll(".cFighters").forEach(el => el.style.display = "none");
+    document.querySelectorAll(".cVehicles").forEach(el => el.style.display = "none");
+    document.querySelectorAll(".cShooters").forEach(el => el.style.display = "unset");
+});
